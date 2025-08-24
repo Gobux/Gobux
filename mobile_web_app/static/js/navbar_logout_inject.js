201cf9ccedcb61d647f06dece973c301297f5b92
Gobux <jason_gobus@hotmail.com>
2025-08-24 21:36:21 +1000

    Add files via upload

---
+++ mobile_web_app/static/js/navbar_logout_inject.js
@@ -0,0 +1,5 @@
+// static/js/navbar_logout_inject.js
+// This script is intentionally left blank. The navigation bar defined in
+// index.html already contains the user email label and logout button, so
+// runtime injection is unnecessary.
+(function(){})();
